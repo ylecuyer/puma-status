75288ac41acae94686d0843838246a0a78afe0b8
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2019-07-23 21:10:44 +0200

    Better: Add help on cli launcher

--- lib/puma-status.rb
+++ lib/puma-status.rb
@@ -3,6 +3,13 @@ require_relative './core.rb'
 
 def run
   debug "puma-status"
+
+  if ARGV.count != 1
+    puts "Call with:"
+    puts "\tpuma-status path/to/puma.state"
+    exit -1
+  end
+
   state_file_path = ARGV[0]
   debug "State file: #{state_file_path}"
   display_stats(get_stats(state_file_path))
